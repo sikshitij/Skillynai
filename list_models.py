016b3e6796f711220df0755ae4880145152e5ecf
sikshitij <singh8181kshitij@gmail.com>
2026-04-07 17:38:12 +0530

    Delete list_models.py

--- list_models.py
+++
@@ -1,9 +0,0 @@
-from google import genai
-from dotenv import load_dotenv
-import os
-load_dotenv()
-
-client = genai.Client(api_key=os.getenv('GEMINI_API_KEY'))
-print("Available models:")
-for m in client.models.list():
-    print("-", m.name)
